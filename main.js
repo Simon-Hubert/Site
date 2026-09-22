96648009aac477fae1d2ec1a5f2f8f41b8dfe6b5
Simon-Hubert <s.hubert.mailpro@gmail.com>
2026-09-22 11:26:02 +0200

    [Add] StationLess

--- main.js
+++ main.js
@@ -84,17 +84,17 @@ function SetList(list, name){
 }
 
 let Projects = [
-    new GameProject("Check Mates", "/pages/VideoGames/CheckMates.html", "/images/CheckMates.png", "Puzzles d'échecs revisités avec de nouvelles règles et des pouvoirs !", "Unity", "2023", "8"),//https://vanillou-39.itch.io/check-mates
-    new GameProject("StationLess", "https://steambotart.itch.io/stationless", "/images/Stationless.png", "Metro Boulot Dodo dans une expérience narrative", "Unity", "2026", "15"),
     new GameProject("Out of Reach", "/pages/VideoGames/OutOfReach.html", "/images/OutOfReach.png", "Jeu VR d'exploration spaciale", "Unreal", "2026", "12"),//https://elfumisto.itch.io/out-of-reach
     new GameProject("Synaptika", "/pages/VideoGames/Synaptika.html", "/images/Synaptika/Title.png", "Jeu VR freelance pour le pôle Léonard De Vinci", "Unity", "2025", "11"),
-    new GameProject("8th Turtle Street", "https://eleanoretht.itch.io/8-turtle-street", "/images/8TurtleStreet.png", "Jeu mobile narratif dans une sorée étudiante", "Unity", "2023", "8"),
-    new GameProject("Lizzzard Wizards", "https://sakripan.itch.io/lizzzard-wizard", "/images/LizzzardWizard.jpg", "Couch Game de coopétition ou des lezards cambriolent une école de magie", "Unreal", "2024", "11"),
     new GameProject("Ava", "/pages/VideoGames/Ava.html", "/images/Ava/Title.png", "Proof of Concept pour le Credit Agricole Assurances, jeu mobile d'aculturation de la jeunesse aux produits d'assurance.", "Godot", "2026", "5"),
-    new Project("Adrian Engine", "Tools", "/pages/Tools/AdrianEngine.html", "/images/AdrianEngine/Components.png", "Moteur de jeu ECS developpé en C++"), //https://github.com/Simon-Hubert/AdrianEnginePublic
-    new Project("Line Circle Intersection", "Maths", "/pages/Maths/LineCircleIntersect/LineCircleIntersectionSolver.html", "/images/LineCircle.png", "Un solveur d'équations mathématiques"),
-    new Project("SceneMap", "Tools", "/pages/Tools/SceneMap.html", "/images/SceneMap.gif", "Outil pour Unity permettant de relier des scenes en utilisant GraphView"),
     new Project("HierarchySequence", "Tools",  "/pages/Tools/HierarchySequence.html", "/images/HierarchySequences/Sequences-1.png", "Outil pour Unity permettant d'organiser une sequence d'action et d'evenements directement depuis la Hierarchie"),
+    new GameProject("Lizzzard Wizards", "https://sakripan.itch.io/lizzzard-wizard", "/images/LizzzardWizard.jpg", "Couch Game de coopétition ou des lezards cambriolent une école de magie", "Unreal", "2024", "11"),
+    new Project("Adrian Engine", "Tools", "/pages/Tools/AdrianEngine.html", "/images/AdrianEngine/Components.png", "Moteur de jeu ECS developpé en C++"), //https://github.com/Simon-Hubert/AdrianEnginePublic
+    new GameProject("StationLess", "/pages/VideoGames/StationLess.html", "/images/Stationless.png", "Metro Boulot Dodo dans une expérience narrative", "Unity", "2026", "15"), //https://steambotart.itch.io/stationless
+    new GameProject("8th Turtle Street", "https://eleanoretht.itch.io/8-turtle-street", "/images/8TurtleStreet.png", "Jeu mobile narratif dans une sorée étudiante", "Unity", "2023", "8"),
+    new GameProject("Check Mates", "/pages/VideoGames/CheckMates.html", "/images/CheckMates.png", "Puzzles d'échecs revisités avec de nouvelles règles et des pouvoirs !", "Unity", "2023", "8"),//https://vanillou-39.itch.io/check-mates
+    new Project("Line Circle Intersection", "Tools", "/pages/Tools/LineCircleIntersect/LineCircleIntersectionSolver.html", "/images/LineCircle.png", "Un solveur d'équations mathématiques"),
+    new Project("SceneMap", "Tools", "/pages/Tools/SceneMap.html", "/images/SceneMap.gif", "Outil pour Unity permettant de relier des scenes en utilisant GraphView"),
 ];
 
 let SocialIcons = [
@@ -104,8 +104,8 @@ let SocialIcons = [
 ];
 
 function GetPageList(active){
-    let PageList = ["Maths", "Tools", "VideoGames"]
-    let extensionList = ["ini", "pkg", "exe"]
+    let PageList = [ "Tools", "VideoGames", "AboutMe"];
+    let extensionList = ["pkg", "exe", "ini"];
 
     let txt = "";
 
